ce5033c8ce189eec0e7ec58e3fcef4efc286be1f
Fauzan <muhdfauzan9302@gmail.com>
2024-09-06 11:35:34 +0800

    chore: add PokemonViewModel

---
+++ app/src/main/java/com/nightcode/pokedex/presentation/ui/info/PokemonViewModel.kt
@@ -0,0 +1,71 @@
+package com.nightcode.pokedex.presentation.ui.info
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.nightcode.pokedex.data.network.model.PokemonDTO
+import com.nightcode.pokedex.data.repository.HomeRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+sealed class CommonState<out T> {
+    data object Idle : CommonState<Nothing>()
+
+    data object Loading : CommonState<Nothing>()
+
+    data class Success<T>(
+        val data: T,
+    ) : CommonState<T>()
+
+    data class Error(
+        val message: String,
+    ) : CommonState<Nothing>()
+}
+
+class PokemonViewModel(
+    private val repository: HomeRepository,
+) : ViewModel() {
+    private val _pokemonUiState = MutableStateFlow<CommonState<List<PokemonDTO>>>(CommonState.Idle)
+    val pokemonUiState: StateFlow<CommonState<List<PokemonDTO>>> = _pokemonUiState
+
+    fun fetchPokemonListAndDetail() {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                _pokemonUiState.value = CommonState.Loading
+                repository
+                    .fetchPokemonList()
+                    .shareIn(
+                        scope = viewModelScope,
+                        started = SharingStarted.WhileSubscribed(),
+                        replay = 1,
+                    ).collect { res ->
+                        res
+                            .onSuccess { data ->
+                                val pokemonList = mutableListOf<PokemonDTO>()
+
+                                data.results.forEach { result ->
+                                    result.name.let { name ->
+                                        pokemonList.add(
+                                            PokemonDTO(
+                                                1,
+                                                name = name,
+                                                imageUrl = result.imageUrl,
+                                                bgColor = "black",
+                                            ),
+                                        )
+                                    }
+                                }
+                                _pokemonUiState.value = CommonState.Success(pokemonList)
+                            }.onFailure { err ->
+                                _pokemonUiState.value =
+                                    CommonState.Error(err.message ?: "An error occurred")
+                            }
+                    }
+            }
+        }
+    }
+}
