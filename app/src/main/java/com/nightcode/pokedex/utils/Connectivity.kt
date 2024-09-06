dc581b6396559c4181a822b3640f189ea3596c9a
Fauzan <muhdfauzan9302@gmail.com>
2024-09-06 11:39:52 +0800

    chore: add Connectivity for checking internet connection

---
+++ app/src/main/java/com/nightcode/pokedex/utils/Connectivity.kt
@@ -0,0 +1,24 @@
+package com.nightcode.pokedex.utils
+
+import timber.log.Timber
+import java.io.IOException
+import java.net.InetSocketAddress
+import javax.net.SocketFactory
+
+object Connectivity {
+    private const val PING_PORT = 53
+    private const val PING_TIMEOUT = 1500
+
+    fun checkInternetConnection(socketFactory: SocketFactory): Boolean =
+        try {
+            Timber.d("Pinging Google")
+            val socket = socketFactory.createSocket() ?: throw IOException("Socket is null.")
+            socket.connect(InetSocketAddress("8.8.8.8", PING_PORT), PING_TIMEOUT)
+            socket.close()
+            Timber.d("Ping success")
+            true
+        } catch (e: IOException) {
+            Timber.e("No internet connection. $e")
+            false
+        }
+}
